030f19cedef58f2eef1761f28216dd773103211b
jlanej <jlanej@gmail.com>
2018-08-01 16:22:25 -0500

    Add stream filtering for loading autosomal regions from bed files

--- ngspca/src/main/java/org/pankratzlab/ngspca/BedUtils.java
+++ ngspca/src/main/java/org/pankratzlab/ngspca/BedUtils.java
@@ -3,6 +3,11 @@ package org.pankratzlab.ngspca;
 import java.io.Closeable;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
 
 import htsjdk.samtools.util.CloseableIterator;
 import htsjdk.tribble.AbstractFeatureReader;
@@ -18,16 +23,75 @@ public class BedUtils {
 	}
 
 	/**
-	 * TODO, region strategies for specific targets
-	 *
+	 * @param file
+	 *            load autosomal {@link BEDFeature}s from this file
+	 * @return {@link List} of {@link BEDFeature}
 	 */
-	enum REGION_STRATEGY {
-		/**
-		 * Load all autosomal regions
-		 */
-		AUTOSOMAL;
+	static List<BEDFeature> loadAutosomal(String file) {
+		return BedUtils.loadAll(file).stream().filter(BedUtils::autosomal).collect(Collectors.toList());
 	}
 
+	/**
+	 * @param file
+	 *            load autosomal {@link BEDFeature}s from this file and convert to
+	 *            UCSC format
+	 * @return {@link Set} of ucsc regions
+	 */
+	static Set<String> loadAutosomalUCSC(String file) {
+		return BedUtils.loadAll(file).stream().filter(BedUtils::autosomal).map(BedUtils::getBedUCSC)
+				.collect(Collectors.toSet());
+	}
+
+	/**
+	 * @param file
+	 *            load all {@link BEDFeature}s in this file
+	 * @return {@link List} of {@link BEDFeature}s
+	 */
+	static List<BEDFeature> loadAll(String file) {
+		BEDFileReader reader = new BEDFileReader(file, false);
+		List<BEDFeature> result = reader.iterator().stream().collect(Collectors.toList());
+		reader.close();
+		return result;
+
+	}
+
+	/**
+	 * @param file
+	 *            load {@link BEDFeature}s from this file
+	 * @param ucscRegions
+	 *            {@link Set} of ucsc formatted regions to load
+	 * @return
+	 */
+	static List<BEDFeature> loadSpecific(String file, Set<String> ucscRegions) {
+		BEDFileReader reader = new BEDFileReader(file, false);
+		List<BEDFeature> result = reader.iterator().stream().filter(bf -> ucscRegions.contains(getBedUCSC(bf)))
+				.collect(Collectors.toList());
+		reader.close();
+		return result;
+
+	}
+
+	/**
+	 * @param bedFeature
+	 * @return UCSC represenation of this {@link BEDFeature}
+	 */
+	static String getBedUCSC(BEDFeature bedFeature) {
+		return bedFeature.getContig() + ":" + bedFeature.getStart() + "-" + bedFeature.getEnd();
+
+	}
+
+	private static boolean autosomal(BEDFeature bedFeature) {
+		String contig = bedFeature.getContig().replaceAll("chr", "");
+		return StringUtils.isNumeric(contig) && Integer.parseInt(contig) < 23;
+	}
+
+	/**
+	 * @param file
+	 *            bed file
+	 * @param requireIndex
+	 *            , if the .tbi index is required for query
+	 * @return {@link BEDFileReader}
+	 */
 	static BEDFileReader getReader(String file, boolean requireIndex) {
 		return new BEDFileReader(file, requireIndex);
 	}
---
+++ ngspca/src/main/java/org/pankratzlab/ngspca/MosdepthBedUtils.java
@@ -0,0 +1,22 @@
+package org.pankratzlab.ngspca;
+
+/**
+ * Process mosdepth bed files for use in PCA
+ *
+ */
+class MosdepthBedUtils {
+
+	/**
+	 * TODO, region strategies for specific targets
+	 *
+	 */
+	enum REGION_STRATEGY {
+		/**
+		 * Load all autosomal regions
+		 */
+		AUTOSOMAL;
+	}
+	
+	
+
+}
