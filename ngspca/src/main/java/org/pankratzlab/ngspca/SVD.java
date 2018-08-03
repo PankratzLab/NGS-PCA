1f33b08ec1786cf708e492cb3e244a2f2586f1ee
jlanej <jlanej@gmail.com>
2018-08-03 09:16:27 -0500

    SVD: no longer retain input matrix

--- ngspca/src/main/java/org/pankratzlab/ngspca/SVD.java
+++ ngspca/src/main/java/org/pankratzlab/ngspca/SVD.java
@@ -17,49 +17,56 @@ class SVD implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 5517545544997813383L;
-	private final DenseMatrix64F dm;
 	private final String[] colNames;
 	private final String[] rowNames;
 
-	// With V,W, and original data M we can always compute U
-	private DenseMatrix64F v;
-	private double[] w;
+	private SvdImplicitQrDecompose_D64 svd;
+
+	/**
+	 * 
+	 * @param colNames
+	 * @param rowNames
+	 */
+	SVD(String[] colNames, String[] rowNames) {
+		super();
+		this.colNames = colNames;
+		this.rowNames = rowNames;
+	}
 
 	/**
 	 * @param dm
 	 *            {@link DenseMatrix64F} that has been pre-normalized, if desired
-	 * @param colNames
-	 * @param rowNames
+	 * @param log
 	 */
-	SVD(DenseMatrix64F dm, String[] colNames, String[] rowNames) {
-		super();
-		this.dm = dm;
-		this.colNames = colNames;
-		this.rowNames = rowNames;
+	void computeSVD(DenseMatrix64F dm, Logger log) {
 		if (dm.getNumCols() != colNames.length) {
 			throw new IllegalArgumentException("Mismatched column lengths");
 		}
 		if (dm.getNumRows() != rowNames.length) {
 			throw new IllegalArgumentException("Mismatched row lengths");
 		}
-	}
-
-	void computeSVD(Logger log) {
-
 		log.info("Computing EJML PCs");
-		SvdImplicitQrDecompose_D64 svd = new SvdImplicitQrDecompose_D64(false, false, true, false);
+		this.svd = new SvdImplicitQrDecompose_D64(false, true, true, false);
 		svd.decompose(dm);
 		log.info("Finished Computing EJML PCs");
 
-		this.v = svd.getV(null, true);
+		log.info("Sorting matrices to descending order");
 
-		DenseMatrix64F tmpW = svd.getW(null);
-		SingularOps.descendingOrder(null, false, tmpW, v, true);
-		int numSingular = Math.min(tmpW.numRows, tmpW.numCols);
-		this.w = new double[numSingular];
-		for (int i = 0; i < numSingular; i++) {
-			w[i] = tmpW.get(i, i);
+		SingularOps.descendingOrder(svd.getU(null, false), false, svd.getW(null), svd.getV(null, true), true);
+
+	}
+
+	/**
+	 * @param numComponents
+	 *            number of singular values to return from the W matrix
+	 * @return
+	 */
+	double[] getSingularValues(int numComponents) {
+		double[] singularValues = new double[numComponents];
+		for (int i = 0; i < numComponents; i++) {
+			singularValues[i] = svd.getW(null).get(i, i);
 		}
+		return singularValues;
 	}
 
 	private void dumpPCsToText(String file) {
