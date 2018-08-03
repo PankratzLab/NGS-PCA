cf69e73f5f3368cb622113bfc0ec3b53f602bb42
jlanej <jlanej@gmail.com>
2018-08-02 20:34:05 -0500

    Remove SVD extracted methods

--- ngspca/src/main/java/org/pankratzlab/ngspca/SimpleNGSPCA.java
+++ ngspca/src/main/java/org/pankratzlab/ngspca/SimpleNGSPCA.java
@@ -8,78 +8,13 @@ import java.util.stream.Collectors;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.math3.linear.DiagonalMatrix;
-import org.apache.commons.math3.linear.MatrixUtils;
-import org.apache.commons.math3.linear.RealMatrix;
 
-import org.ejml.alg.dense.decomposition.svd.SvdImplicitQrDecompose_D64;
-import org.ejml.data.DenseMatrix64F;
-import org.ejml.ops.SingularOps;
 import org.pankratzlab.ngspca.MosdepthUtils.REGION_STRATEGY;
 
 /**
  * A simplified version of BamImport that uses MosDepth output to generate PCS
  */
 public class SimpleNGSPCA implements Serializable {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 5517545544997813383L;
-	private final RealMatrix m;
-	private final String[] colNames;
-	private final String[] rowNames;
-
-	// With V,W, and original data M we can always compute U
-	private RealMatrix v;
-	private DiagonalMatrix w;
-
-	/**
-	 * @param m
-	 * @param colNames
-	 * @param rowNames
-	 */
-	private SimpleNGSPCA(RealMatrix m, String[] colNames, String[] rowNames) {
-		super();
-		this.m = m;
-		this.colNames = colNames;
-		this.rowNames = rowNames;
-		if (m.getColumnDimension() != colNames.length) {
-			throw new IllegalArgumentException("Mismatched column lengths");
-		}
-		if (m.getRowDimension() != rowNames.length) {
-			throw new IllegalArgumentException("Mismatched row lengths");
-		}
-	}
-
-	private void computeSVD(Logger log) {
-		log.info("computing SVD base");
-
-		DenseMatrix64F a = NormalizationOperations.toDenseMatrix64F(m);
-		log.info("Computing EJML PCs");
-		SvdImplicitQrDecompose_D64 svd = new SvdImplicitQrDecompose_D64(false, false, true, false);
-		svd.decompose(a);
-		log.info("Finished Computing EJML PCs");
-
-		log.info("finished computing SVD base");
-
-		DenseMatrix64F tv = svd.getV(null, true);
-
-		DenseMatrix64F tmpW = svd.getW(null);
-		SingularOps.descendingOrder(null, false, tmpW, tv, true);
-		int numSingular = Math.min(tmpW.numRows, tmpW.numCols);
-		double[] singularValues = new double[numSingular];
-		for (int i = 0; i < numSingular; i++) {
-			singularValues[i] = tmpW.get(i, i);
-		}
-		this.w = new DiagonalMatrix(singularValues);
-		this.v = MatrixUtils.createRealMatrix(tv.numRows, tv.numCols);
-		for (int row = 0; row < tv.numRows; row++) {
-			for (int col = 0; col < tv.numCols; col++) {
-				v.addToEntry(row, col, tv.get(row, col));
-			}
-		}
-	}
 
 	private void dumpPCsToText(String file) {
 		//
