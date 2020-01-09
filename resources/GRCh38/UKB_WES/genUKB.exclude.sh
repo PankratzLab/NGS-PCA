2080de5d7e4c06a2f565e846cdfaa44881ceaf30
jlanej <jlanej@gmail.com>
2020-01-09 09:44:24 -0600

    prevent negative starts in 20kb buffer

--- resources/GRCh38/UKB_WES/genUKB.exclude.sh
+++ resources/GRCh38/UKB_WES/genUKB.exclude.sh
@@ -8,11 +8,12 @@
 # tabix xgen_plus_spikein.b38.bed.gz
 
 
-cat xgen_plus_spikein.b38.bed.gz |zcat |awk '{print "chr"$1"\t"$2-20000"\t"$3+20000}' |bgzip >xgen_plus_spikein.b38.chr.bed.gz
+cat xgen_plus_spikein.b38.bed.gz |zcat |awk '{if($2-20000 <0) print "chr"$1"\t"0"\t"$3+20000; else print "chr"$1"\t"$2-20000"\t"$3+20000}' |bgzip >xgen_plus_spikein.b38.chr.bed.gz
 cat ../ngs_pca_exclude.sv_blacklist.map.kmer.50.1.0.dgv.gsd.sorted.merge.bed.gz xgen_plus_spikein.b38.chr.bed.gz|zcat |bedtools sort |bedtools merge |bgzip >ngs_pca_exclude.sv_blacklist.map.kmer.50.1.0.dgv.gsd.xgen.sorted.merge.bed.gz
 tabix ngs_pca_exclude.sv_blacklist.map.kmer.50.1.0.dgv.gsd.xgen.sorted.merge.bed.gz
 
 
 
 
-awk '{if($2-20000 <0 0) print "chr"$1"\t"0"\t"$3+20000; else print "chr"$1"\t"$2-20000"\t"$3+20000}'
+
+
